c03cf51248a74336adf0af3304ca45f83ce031b0
Jose Acosta <jfacosta101@gmail.com>
2025-07-08 02:19:29 -0700

    Changing back to render api for now

--- src/js/utils/config.js
+++ src/js/utils/config.js
@@ -2,17 +2,17 @@
 // This works across all platforms (Mac, Windows, iOS, Android, etc.)
 export const config = {
     // API URL - change this to switch between local and production
-    //API_URL: 'https://ygopyguy.onrender.com',
+    API_URL: 'https://ygopyguy.onrender.com',
     
     // You can also set it to local for development:
-    API_URL: 'http://127.0.0.1:8081',
+    //API_URL: 'http://127.0.0.1:8081',
     
     // Other configuration options
     APP_VERSION: '2.1.0',
     APP_NAME: 'YGO Ripper UI v2',
     
     // Timeouts and limits
-    API_TIMEOUT: 30000,
+    API_TIMEOUT: 120000,
     CACHE_TTL: 3600000, // 1 hour
     MAX_CACHE_SIZE: 1000
 };
