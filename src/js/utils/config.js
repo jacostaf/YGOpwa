cae4c824b662dd663f64f6a976dd3f8b2422e0aa
jacostaf <64145802+jacostaf@users.noreply.github.com>
2025-08-08 02:21:00 -0700

    Update config.js

--- src/js/utils/config.js
+++ src/js/utils/config.js
@@ -4,7 +4,7 @@
 export const config = {
     // API URL - TCGcsv Implementation
     // Using TCGcsv as the sole data source (no MongoDB required)
-    API_URL: 'http://127.0.0.1:8083',
+    API_URL: 'https://ygopyguy.onrender.com',
     
     // Other configuration options
     APP_VERSION: '2.1.0',
